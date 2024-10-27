37a77a986fa08fad0ac22d60a7f6bddc24a6d257
BZDSM99 <bzdsm99@gmail.com>
2024-10-27 23:21:05 +0800

    文件路径优化

--- main/GameConfig.py
+++ main/GameConfig.py
@@ -18,24 +18,24 @@ BACKGROUND_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "background.png")  
 ICON_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "icon.png")              #标题图标
 
 # 飞机大战标题图片
-TITLE_IMAGE = "../images/name.png"
+TITLE_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "name.png")
 # 开始游戏图片
-START_IMAGE = "../images/start_nor.png"
+START_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "start_nor.png")
 # 开始游戏加载动画图片
-LOADING_IMAGE1 = "../images/game_loading1.png"
-LOADING_IMAGE2 = "../images/game_loading2.png"
-LOADING_IMAGE3 = "../images/game_loading3.png"
-LOADING_IMAGE4 = "../images/game_loading4.png"
+LOADING_IMAGE1 = os.path.join(CURRENT_DIR, "..", "images", "game_loading1.png")
+LOADING_IMAGE2 = os.path.join(CURRENT_DIR, "..", "images", "game_loading2.png")
+LOADING_IMAGE3 = os.path.join(CURRENT_DIR, "..", "images", "game_loading3.png")
+LOADING_IMAGE4 = os.path.join(CURRENT_DIR, "..", "images", "game_loading4.png")
 
 PLANE1_IMG = os.path.join(CURRENT_DIR, "..", "images", "hero1.png")
 PLANE2_IMG = os.path.join(CURRENT_DIR, "..", "images", "hero2.png")
 
 
 # 开始/暂停游戏图片
-PAUSE_NOR = "../images/pause_nor.png"
-PAUSE_PRESSED = "../images/pause_pressed.png"
-RESUME_NOR = "../images/resume_nor.png"
-RESUME_PRESSED = "../images/resume_pressed.png"
+PAUSE_NOR = os.path.join(CURRENT_DIR, "..", "images", "pause_nor.png")
+PAUSE_PRESSED = os.path.join(CURRENT_DIR, "..", "images", "pause_pressed.png")
+RESUME_NOR = os.path.join(CURRENT_DIR, "..", "images", "resume_nor.png")
+RESUME_PRESSED = os.path.join(CURRENT_DIR, "..", "images", "resume_pressed.png")
 
 # 小型敌机图片
 SMALL_EMEMY_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "enemy0.png")
@@ -83,25 +83,25 @@ B_ENEMY_HIT_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "enemy2_hit.png")
 FONT_PATH = os.path.join(CURRENT_DIR, "..", "font", "font.ttf")
 
 # 超级炸弹
-BOMB_IMAGE = "../images/bomb.png"
+BOMB_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "bomb.png")
 
 # 补给(超级子弹/超级炸弹)
-PROP1_IMAGE = "../images/prop_type_0.png"
-PROP2_IMAGE = "../images/prop_type_1.png"
+PROP1_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "prop_type_0.png")
+PROP2_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "prop_type_1.png")
 
 # 普通/超级子弹图片
-BUTTLE1_IMAGE = "../images/bullet1.png"
+BUTTLE1_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "bullet1.png")
 # 超级子弹图片
-BUTTLE2_IMAGE = "../images/bullet2.png"
+BUTTLE2_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "bullet2.png")
 
 # 我方飞机生命图片
-LIFE_IMAGE = "../images/life.png"
+LIFE_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "life.png")
 
 
 # 结束游戏图片
-GAMEOVER_IMAGE = "../images/quit_nor.png"
+GAMEOVER_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "quit_nor.png")
 # 重新开始游戏图片
-RESTART_IMAGE = "../images/restart_nor.png"
+RESTART_IMAGE = os.path.join(CURRENT_DIR, "..", "images", "restart_nor.png")
 
 # try:
 #     with open(BACKGROUND_IMAGE, 'rb') as f:
